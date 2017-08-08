1d54a3b4726580bc8c662dc3ba0cc2d100772ac7
WOOCHUL\uchulkim <kwc7171@gmail.com>
2017-08-08 16:13:42 +0900

    서보모터

--- servo_test.c
+++ servo_test.c
@@ -3,34 +3,34 @@
 #include <softPwm.h>
 #include <unistd.h>
 
-#define SERVO 1
+#define SERVO 1 //gpio ¹øÈ£
 
 int main()
 {
   char str;
   
-  if(wiringPiSetup() == 1)
+  if(wiringPiSetup() == -1)
     return 1;
   softPwmCreate(SERVO, 0, 200);
   
   while(1)
   {
-    fputs("select r, l, 1 : ", stdout);
+    fputs("select r, l, q : ", stdout);
     scanf("%c", &str);
     getchar();
     if(str == 'r')
     {
       softPwmWrite(SERVO, 24);
-      usleep(50000);
+      usleep(5000);
       softPwmWrite(SERVO, 0);
     }
-    else if(str =='l')
+    else if(str == 'l')
     {
-      softPwmWrite(SERVO, %);
-      usleep(50000);
+      softPwmWrite(SERVO, 5);
+      usleep(5000);
       softPwmWrite(SERVO, 0);
       }
       else if ( str == 'q') return 0;
       }
-      return0;
+      return 0;
 }  
